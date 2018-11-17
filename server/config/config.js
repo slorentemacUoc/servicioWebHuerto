7825aad17cd6c286cc9fe5edcc966baccf6cf135
sara <sara_3000@msn.com>
2018-11-17 12:38:11 +0100

    Varible heroku

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/huertoUrbano'
 }else{
-    urlDB = 'mongodb://huerto-user:102010Sara@ds029911.mlab.com:29911/huertourbano'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
